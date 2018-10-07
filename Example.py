464fdfe2536d93a04aa822a45838aad8b0cd4b85
Winnie Jeng <winniewjeng@gmail.com>
2018-10-06 23:18:01 -0700

    still working on raise, try, except, sys, traceback
    '

--- Example.py
+++ Example.py
@@ -1,7 +1,6 @@
 
 from ExampleException import *
 
-
 # Base Class
 class ExampleBase:
 
@@ -35,12 +34,13 @@ class Useful(ExampleBase):
         # turn the dictionary's tuple into list in order to perform arithmetic
         # then traverse through the dictionary's tuples.
         # items[0] is _number_of_shares and item[1] is _price inside the tuple
-        for items in list(self.stocks.values()):
+        for item in list(self.stocks.values()):
             # if _number_of_shares is negative, raise Exception
             try:
-                product = items[0] * items[1]
-                if items[0] < 0:
-                    raise ExampleException("Invalid number of shares!")
+                product = item[0] * item[1]
+                if item[0] < 0:
+                    raise ExampleException("ERROR: Invalid number of shares")
+            # catch the exception so program can output error and continue to execute
             except ExampleException as error:
                 print(error.message)
             # sum up each term of the stock tuple
@@ -50,7 +50,13 @@ class Useful(ExampleBase):
     # string method overriding the one from base class
     # outputs a table of stock dictionary and total stock value
     def __str__(self):
-        for numbers_of_shares, price in self.stocks.items():
-            print("On", numbers_of_shares, price[0], "stocks are purchased at $", price[1])
+        for date, item in self.stocks.items():
+            if item[0] == 1:
+                print("On", date, item[0], "share is purchased at $", item[1])
+            elif item[0] > 1:
+                print("On", date, item[0], "shares are purchased at $", item[1])
+            else:
+                e = ExampleException("INVALID shares")
+                print("On", date, e, "are purchased at $", item[1])
 
         return "Company's Symbol " + self.company_name
--- lab2_exceptions.py
+++ lab2_exceptions.py
@@ -1,5 +1,7 @@
 
 from Example import *
+from ExampleException import *
+import sys, traceback
 
 if __name__ == "__main__":
 
@@ -9,25 +11,49 @@ if __name__ == "__main__":
         "10-11-2020": (1, 21.45),
     }
 
-    # instantiate object stock
-    stock_holds1 = Useful("GOOG", stocks_dict)
+    # instantiate object stock GOOG
+    stock_hold1 = Useful("GOOG", stocks_dict)
 
-    # second stock dictionary to demonstrate other in add_purchase()
+    # more_stock_dict to demonstrate input argument other in add_purchase()
     more_stocks_dict = {
         "10-12-2020": (3, 11.00),
         "10-13-2020": (4, 22.00)
     }
 
-    # one stock dictionary to demonstrate **kwargs in add_purchase()
-    one_stock = {"10-14-2020": (1, 10.20)}
+    # one_stock dict to demonstrate input argument **kwargs in add_purchase()
+    one_more_stock_dict = {"10-14-2020": (1, 10.20)}
 
     # demonstrate add_purchase()
-    stock_holds1.add_purchase(more_stocks_dict, **one_stock)
+    stock_hold1.add_purchase(more_stocks_dict, **one_more_stock_dict)
 
     # print(example.stocks)
 
-    stock_holds2 = Useful("AMAZ", stocks_dict)
-    print(stock_holds2)
+    stock_hold2 = Useful("AMAZ", stocks_dict)
+    print(stock_hold2)
+
+    # total value of the stocks is assigned to a float variable called value
+    value = stock_hold2.compute_value()
 
     # output the total stock value
-    print("total value: ", stock_holds2.compute_value())
+    print("total value: ", round(value, 2))
+
+    print()
+
+    # create an invalid_stock_dict where the number of share is negative
+    valid_stock_dict = {"10-14-2020": (-1, 0.38)}
+    invalid_stock_dict = {"10-15-2020": (-4, 0.85),
+                          "10-16-2020": (-7, 0.55),
+                          "10-17-2020": (-3, 0.15)}
+    stock_hold3 = Useful("TSLA", valid_stock_dict)
+
+    try:
+        # add the invalid_stock_dict to the constructor's dictionary self.stocks
+        stock_hold3.add_purchase(invalid_stock_dict)
+    except ExampleException as e:
+        # print(e.message)
+        print("hihihhihihihihi")
+        print(repr(e.message))
+
+    print(stock_hold3)
+    val = stock_hold3.compute_value()
+    print("total value: ", round(val, 2))
