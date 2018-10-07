207d1543e9e686250ac8b9766fbab8980d410531
Winnie Jeng <winniewjeng@gmail.com>
2018-10-06 23:47:25 -0700

    implement sys and traceback

--- Example.py
+++ Example.py
@@ -29,34 +29,26 @@ class Useful(ExampleBase):
     # go through the self.stocks dictionary and calculate total value
     # as summation of shared purchase values multiplied by stock price
     def compute_value(self):
-        # initializes value of the total stock value to 0
-        sum = 0
-        # turn the dictionary's tuple into list in order to perform arithmetic
-        # then traverse through the dictionary's tuples.
-        # items[0] is _number_of_shares and item[1] is _price inside the tuple
+        value_sum = 0
         for item in list(self.stocks.values()):
-            # if _number_of_shares is negative, raise Exception
-            try:
-                product = item[0] * item[1]
-                if item[0] < 0:
-                    raise ExampleException("ERROR: Invalid number of shares")
-            # catch the exception so program can output error and continue to execute
-            except ExampleException as error:
-                print(error.message)
-            # sum up each term of the stock tuple
-            sum += product
-        return sum
+            product = item[0] * item[1]
+            value_sum += product
+            if item[0] < 0:
+                raise ExampleException("ERROR: Invalid number of shares!!")
+        return value_sum
 
     # string method overriding the one from base class
     # outputs a table of stock dictionary and total stock value
     def __str__(self):
+        print("Company's Symbol " + self.company_name)
         for date, item in self.stocks.items():
             if item[0] == 1:
-                print("On", date, item[0], "share is purchased at $", item[1])
+                print("On {} {} share is purchased at ${}".format(date, item[0], item[1]))
             elif item[0] > 1:
-                print("On", date, item[0], "shares are purchased at $", item[1])
+                print("On {} {} shares are purchased at ${}".format(date, item[0], item[1]))
             else:
-                e = ExampleException("INVALID shares")
-                print("On", date, e, "are purchased at $", item[1])
+                e = ExampleException("!INVALID!")
+                print("On {} {} shares are purchased at ${}".format(date, e, item[1]))
 
-        return "Company's Symbol " + self.company_name
+        return "------------------------------------------------------"
+
--- lab2_exceptions.py
+++ lab2_exceptions.py
@@ -5,34 +5,30 @@ import sys, traceback
 
 if __name__ == "__main__":
 
-    # stock dictionary, key string date, value tuple (shares,price)
-    stocks_dict = {
-        "10-10-2020": (2, 20.50),
-        "10-11-2020": (1, 21.45),
-    }
+    """Some Stocks Dictionaries"""
+    # ========================================================================== #
 
-    # instantiate object stock GOOG
-    stock_hold1 = Useful("GOOG", stocks_dict)
+    stocks_dict = {"10-10-2020": (2, 20.50), "10-11-2020": (1, 21.45)}
 
-    # more_stock_dict to demonstrate input argument other in add_purchase()
-    more_stocks_dict = {
-        "10-12-2020": (3, 11.00),
-        "10-13-2020": (4, 22.00)
-    }
+    more_stocks_dict = {"10-12-2020": (3, 11.00), "10-13-2020": (4, 22.00)}
 
-    # one_stock dict to demonstrate input argument **kwargs in add_purchase()
     one_more_stock_dict = {"10-14-2020": (1, 10.20)}
 
-    # demonstrate add_purchase()
-    stock_hold1.add_purchase(more_stocks_dict, **one_more_stock_dict)
+    valid_stock_dict = {"10-15-2020": (1, 0.38)}
 
-    # print(example.stocks)
+    invalid_stock_dict = {"10-16-2020": (-4, 0.85), "10-17-2020": (7, 0.55)}
 
-    stock_hold2 = Useful("AMAZ", stocks_dict)
-    print(stock_hold2)
+    # ========================================================================== #
+
+    # instantiate object
+    stock_hold_1 = Useful("AMAZ", stocks_dict)
+
+    stock_hold_1.add_purchase(more_stocks_dict, **one_more_stock_dict)
 
     # total value of the stocks is assigned to a float variable called value
-    value = stock_hold2.compute_value()
+    value = stock_hold_1.compute_value()
+
+    print(stock_hold_1)
 
     # output the total stock value
     print("total value: ", round(value, 2))
@@ -40,20 +36,14 @@ if __name__ == "__main__":
     print()
 
     # create an invalid_stock_dict where the number of share is negative
-    valid_stock_dict = {"10-14-2020": (-1, 0.38)}
-    invalid_stock_dict = {"10-15-2020": (-4, 0.85),
-                          "10-16-2020": (-7, 0.55),
-                          "10-17-2020": (-3, 0.15)}
-    stock_hold3 = Useful("TSLA", valid_stock_dict)
+
+    stock_hold_2 = Useful("TSLA", valid_stock_dict)
+    stock_hold_2.add_purchase(invalid_stock_dict)
 
     try:
-        # add the invalid_stock_dict to the constructor's dictionary self.stocks
-        stock_hold3.add_purchase(invalid_stock_dict)
+        val = stock_hold_2.compute_value()
+        print(stock_hold_2)
+        print("total value: ", round(val, 2))
     except ExampleException as e:
-        # print(e.message)
-        print("hihihhihihihihi")
+        print(stock_hold_2)
         print(repr(e.message))
-
-    print(stock_hold3)
-    val = stock_hold3.compute_value()
-    print("total value: ", round(val, 2))
